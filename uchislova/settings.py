b071e91fda2a9c000b008ddd0b170468eb231086
swasher <mr.swasher@gmail.com>
2016-03-20 19:16:34 +0000

    fix path

--- uchislova/settings.py
+++ uchislova/settings.py
@@ -139,10 +139,10 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 # heroku: static
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'collectstatic')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, "../uchislova/collectstatic"),
+    #"static",
     'bower_components',
 ]
 
